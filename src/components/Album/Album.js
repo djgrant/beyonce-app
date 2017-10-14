75dadb1590d631438511e427c6ea28489067562a
Daniel Grant <danieljohngrant@gmail.com>
2017-10-14 11:03:53 +0100

    Add starting point album component

---
+++ src/components/Album/Album.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './Album.css';
+
+const Album = props => (
+  <div>
+    <h2>{props.album}</h2>
+    <img src={props.imageUrl} />
+    <p>Release date: {props.releaseDate}</p>
+    <p>Genre: {props.genre}</p>
+  </div>
+);
+
+Album.propTypes = {
+  album: propTypes.string.isRequired,
+  imageUrl: propTypes.string.isRequired,
+  releaseDate: propTypes.string.isRequired,
+  genre: propTypes.string.isRequired
+};
+
+export default App;
---
+++ src/components/Albums/Albums.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Albums extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+Albums.propTypes = {
+
+};
+
+export default Albums;
