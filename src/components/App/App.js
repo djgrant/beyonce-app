ca43608e30373eda11fd9ecb9230940b356d4cc6
Islam Dudaev <islamwolf23@gmail.com>
2018-05-26 21:03:17 +0100

    Add nav and links for separate pages in Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Albums from "../Albums/Albums";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Section1 from "../Section1"
-import Section2 from "../Section2"
-import Section3 from "../Section3"
+import Section1 from "../Section1";
+import Section2 from "../Section2";
+import Section3 from "../Section3";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Section1 />
-          <Section2 />
-          <Section3 />
-          
-          
+          <ul>
+            <li>
+              <Link to="/">Beyonce</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/albums">Albums</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Section1} />
+          <Route path="/about" component={Section2} />
+          <Route path="/albums" component={Section3} />
         </div>
       </Router>
     );
