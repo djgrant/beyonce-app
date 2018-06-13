fa047995387e0c75cefddedef931c5c1550dca64
Awet Fessha <awetkifle94@gmail.com>
2018-06-13 21:34:59 +0100

    Fetched the album

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 class Albums extends Component {
-  render() {
-    return (
-      <div></div>
+  constructor() {
+    super();
+    this.state = {
+      albums: []
+    };
+
+    fetch(
+      "https://rawgit.com/rarmatei/f5ae92ac93d9716affab822a3f54f95b/raw/e62641b3f5ddd12c4fe34aa0912488224594e5a7/beyonce-albums.json"
     )
+      .then(data => data.json())
+      .then(data => {
+        console.log(data.collectionName);
+        const albumList = data.map(result => result.collectionName);
+        this.setState = {
+          albums: albumList
+        };
+      });
+  }
+  render() {
+    return <div />;
   }
 }
 
-Albums.propTypes = {
-
-};
+Albums.propTypes = {};
 
 export default Albums;
